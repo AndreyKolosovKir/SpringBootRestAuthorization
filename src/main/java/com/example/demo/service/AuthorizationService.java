7b46faad0fc5be0f19b5754b6a37b1c5dc6c02db
AndreyKolosovKir <andrey.e.04@gmail.com>
2023-09-10 15:14:02 +0300

    Second commit

--- src/main/java/com/example/demo/service/AuthorizationService.java
+++ src/main/java/com/example/demo/service/AuthorizationService.java
@@ -16,16 +16,10 @@ public class AuthorizationService {
 
     public List<Authorities> getAuthorities(String user, String password) {
         if (isEmpty(user) || isEmpty(password)) {
-            //TODO Вам нужно написать обработчики ошибок, которые выкидывает сервис AuthorizationService:
-
-            //TODO 1) на InvalidCredentials он должен отсылать обратно клиенту HTTP-статус с кодом 400
-            // и телом в виде сообщения из exception;
             throw new InvalidCredentials("User name or password is empty");
         }
         List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
         if (isEmpty(userAuthorities)) {
-            //TODO 2) на UnauthorizedUser он должен отсылать обратно клиенту HTTP-статус с кодом 401
-            // телом в виде сообщения из exception и писать в консоль сообщение из exception.
             throw new UnauthorizedUser("Unknown user " + user);
         }
         return userAuthorities;
